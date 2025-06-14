c70b504b025db4e8d30a54884cce615994c8be09
sodium carbonate <webitproff@gmail.com>
2025-06-14 08:48:00 +0300

    Update prismhighlight.en.lang.php

--- prismhighlight/lang/prismhighlight.en.lang.php
+++ prismhighlight/lang/prismhighlight.en.lang.php
@@ -3,6 +3,4 @@ defined('COT_CODE') or die('Wrong URL');
 
 $L['prismhighlight'] = 'Prism Highlighter';
 $L['prismhighlight_desc'] = 'Client-side syntax highlighting with Prism.js';
-$L['prismhighlight_theme'] = 'Prism theme';
-$L['prismhighlight_languages'] = 'Supported languages';
-$L['prismhighlight_use_cdn'] = 'Use CDN';
+$L['cfg_theme'] = 'Prism theme';
