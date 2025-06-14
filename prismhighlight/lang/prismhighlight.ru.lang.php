f3173c78134bccfcda688ed43c7389baaadc2241
sodium carbonate <webitproff@gmail.com>
2025-06-14 08:47:40 +0300

    Update prismhighlight.ru.lang.php

--- prismhighlight/lang/prismhighlight.ru.lang.php
+++ prismhighlight/lang/prismhighlight.ru.lang.php
@@ -3,6 +3,4 @@ defined('COT_CODE') or die('Wrong URL');
 
 $L['prismhighlight'] = 'Prism Подсветка';
 $L['prismhighlight_desc'] = 'Подсветка синтаксиса на стороне клиента с помощью Prism.js';
-$L['prismhighlight_theme'] = 'Тема Prism';
-$L['prismhighlight_languages'] = 'Поддерживаемые языки';
-$L['prismhighlight_use_cdn'] = 'Использовать CDN';
+$L['cfg_theme'] = 'Тема Prism';
